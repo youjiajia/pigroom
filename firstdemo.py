09ad544ab74c04cee5cd7f79825cb247eccd8a91
Mr、佳少 <yjj982288470@gmail.com>
2016-11-22 00:01:28 +0800

    demo

---
+++ firstdemo.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#   Author  :   jiajia you
+#   E-mail  :   hi_youjiajia@163.com
+#   Date    :   16/11/21 22:54:18
+#   Desc    :
+#
+
+
+import json
+import os
+import signal
+
+from tornado.gen import engine, Task
+from tornado.ioloop import IOLoop
+from tornado.web import Application
+from tornado.websocket import WebSocketHandler
+
+import tornadoredis
+
+
+class RoomDemo(WebSocketHandler):
+    FINISH_MSG = json.dumps(dict(type='state', data='finish'))
+
+    def initialize(self, **kws):
+        self.client = tornadoredis.Client(host=kws["REDIS_HOST"], port=kws["REDIS_PORT"])
+
+    @engine
+    def listen(self, channels):
+        self.client.connect()
+        yield Task(self.client.subscribe, channels)
+        self.client.listen(self.on_subscribe)
+
+    def open(self, room_id, *args, **kws):
+        self.write_message(json.dumps(dict(type='state', data="in")))
+        self.channels = "room:" + str(room_id)
+        self.room_id = room_id
+        self.listen(self.channels)
+
+    def on_message(self, message):
+        self.write_message(json.dumps(dict(type='message', data=message)))
+
+    def on_subscribe(self, msg):
+        self.write_message(msg.body)
+        if "finish" in msg and "state" in msg:
+            self.close(1000)
+
+    def on_close(self):
+        if not self.client.subscribed:
+            return
+        self.client.unsubscribe(self.channels)
+        self.client.disconnect()
+
+    def check_origin(self, origin):
+        return True
+
+
+def init_app(cfg):
+    app = Application([
+        (r"/pigroom/ws/(\d+)", RoomDemo, cfg),
+    ])
+    return app
+
+
+def load_from_obj(objname):
+    import config
+    obj = getattr(config, objname)
+    d = dict()
+    for key in dir(obj):
+        if key.isupper():
+            d[key] = getattr(obj, key)
+    return d
+
+
+def load_config():
+    cfg = load_from_obj("DefaultConfig")
+    return cfg
+
+
+def stop():
+    IOLoop.current().stop()
+    print "server stopped."
+
+
+def signal_handler(signum, frames):
+    IOLoop.current().add_callback_from_signal(stop)
+
+
+def main(debug=False):
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    cfg = load_config()
+    host = cfg.get("SERVER_HOST", '0.0.0.0')
+    port = cfg.get("SERVER_PORT", 11000)
+
+    app = init_app(cfg)
+    if debug:
+        return app
+
+    print "server listen on %s:%d" % (host, port)
+    app.listen(port, address=host)
+
+    print "server starts..."
+    IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
