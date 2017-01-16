2ae5b95a73f1222b71cd8725f640d18f4b405c67
yjj <youjiajia@wonxing.com>
2017-01-16 21:17:54 +0800

    yjj

--- chatroom/__init__.py
+++ chatroom/__init__.py
@@ -42,7 +42,6 @@ def teardown_request(exception):
     if not MYDB.is_closed():
         MYDB.close()
 
-
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
