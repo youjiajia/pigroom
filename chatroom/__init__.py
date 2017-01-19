bfc2b274247f4f031178f0b02d462775bccaa4d9
yjj <youjiajia@wonxing.com>
2017-01-19 17:12:43 +0800

    yjj

--- chatroom/__init__.py
+++ chatroom/__init__.py
@@ -43,6 +43,7 @@ def teardown_request(exception):
 
 
 @app.after_request
+
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
