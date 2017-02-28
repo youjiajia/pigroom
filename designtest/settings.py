7eb7811d9c66c7af6c95cb5029a2e68639c6e198
JasonYou <yjj982288470@gmail.com>
2017-02-28 23:30:17 +0800

    test

--- designtest/settings.py
+++ designtest/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'designtest.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'designtest.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
