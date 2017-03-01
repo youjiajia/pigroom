37f9425d6ff15f226b1cefb0e08817a7889506e0
JasonYou <yjj982288470@gmail.com>
2017-03-01 23:36:17 +0800

    gitinore

--- manage.py
+++ manage.py
@@ -6,5 +6,10 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
+    if sys.argv[1] == "runserver" and len(sys.argv) == 2:
+        import settings
+        host = "{0}:{1}".format(settings.SERVER_HOST,
+                                settings.SERVER_HOST)
+        sys.argv.append(settings.SERVER_HOST)
     print sys.argv
     execute_from_command_line(sys.argv)
