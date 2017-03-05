176f00cdb031f956f0dcc0eafe024575daf9dbbe
JasonYou <yjj982288470@gmail.com>
2017-03-05 14:25:40 +0800

    views

--- urls.py
+++ urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from chatroom.views import sendmail
+from chatroom.views.views import sendmail
 
 urlpatterns = [
     url(r'^test/', sendmail),
