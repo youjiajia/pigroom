fa28930e9c71fa79d6a4f70031603b97c338e00e
JasonYou <yjj982288470@gmail.com>
2017-03-06 23:45:48 +0800

    yjj

--- chatroom/models/user.py
+++ chatroom/models/user.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django
 
 
 class UserProfile(models.Model):
@@ -11,8 +12,13 @@ class UserProfile(models.Model):
     cover = models.CharField(max_length=200, default='', blank=True)
     friendcount = models.IntegerField(default=0, blank=True)
     gender = models.IntegerField(default=0, blank=True)
-    update_at = models.DateTimeField(auto_now=True)
-    create_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "auth_userprofile"
+        app_label = "chatroom"created
+
+    def __unicode__(self):
+        return self.nickname
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
--- settings.py
+++ settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'wsgi.application'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'PAGINATE_BY': 10
 }
 
